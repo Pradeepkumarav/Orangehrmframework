a52d51b18b260e97c1cce8f1fbd792181f4a3b16
Pradeepkumarav <pradeep7725@gmail.com>
2023-08-21 19:51:47 +0530

    changes

---
+++ src/test/java/newpackage/now.java
@@ -0,0 +1,12 @@
+package newpackage;
+
+public class now {
+
+	public static void main(String[] args) {
+
+		System.out.println("hii");
+		System.out.println("kumarapradeep");
+
+	}
+
+}
---
+++ src/test/java/newpackage/utility.java
@@ -0,0 +1,11 @@
+package newpackage;
+
+public class utility {
+
+	public static void main(String[] args) {
+		System.out.println("utility");
+		System.out.println("hii");
+
+	}
+
+}
