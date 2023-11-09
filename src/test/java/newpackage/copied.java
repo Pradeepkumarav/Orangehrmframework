9b52e497663b3814baf90675475ab9c243fb30f1
Pradeepkumarav <pradeep7725@gmail.com>
2023-11-09 10:50:50 +0530

    "gg"

--- src/test/java/newpackage/copied.java
+++ src/test/java/newpackage/copied.java
@@ -4,7 +4,7 @@ public class copied {
 
 	public static void main(String[] args) {
 		System.out.println("new");
-		System.out.println("old");
+		System.out.println("old modified");
               //hii
 
 	}
